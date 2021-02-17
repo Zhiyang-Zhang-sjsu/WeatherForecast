cb40d4702fc3a9b2a19614243144cfb129b632e9
Zhiyang-Zhang-sjsu <43052894+Zhiyang-Zhang-sjsu@users.noreply.github.com>
2021-02-16 19:37:57 -0800

    Add files via upload

---
+++ controllers.js
@@ -0,0 +1,32 @@
+// Controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+  $scope.city = cityService.city;
+  
+  $scope.$watch('city', function() {
+      cityService.city = $scope.city; 
+  });
+    
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+  $scope.city = cityService.city;
+  
+  $scope.days = '8';
+  if ($routeParams.days !== undefined) {
+    $scope.days = (parseInt($routeParams.days) * 8).toString();
+  }
+
+  $scope.mykey = "665a8b912e13058b73842b1d97df7e84"
+  
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?");
+  $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days, appid: $scope.mykey});
+  console.log($scope.weatherResult);
+  
+  $scope.convertToFahrenheit = function(degK) {
+    return Math.round((1.8 * (degK - 273)) + 32);      
+  }
+
+  $scope.convertToDate = function(dt) { 
+    return new Date(dt * 1000);  
+  };
+}]);
